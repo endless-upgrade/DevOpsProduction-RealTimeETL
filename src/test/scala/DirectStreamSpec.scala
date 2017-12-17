74c4f5e6f1efc859909075286c54da94a06ad388
d.pasquali <d.pasquali@reply.it>
2017-12-17 15:58:19 +0100

    pending test

--- src/test/scala/DirectStreamSpec.scala
+++ src/test/scala/DirectStreamSpec.scala
@@ -13,42 +13,43 @@ class DirectStreamSpec extends FlatSpec with BeforeAndAfterAll{
 
   override def beforeAll(): Unit = {
     super.beforeAll()
-
-    println("\n\n\nSTART CONFLUENT")
-
-    var startConfluent = "sudo /opt/confluent-3.3.0/bin/confluent start schema-registry" !!
-
-    println("-----------")
-    println(startConfluent)
-    println("-----------")
-
-    if(startConfluent.contains("DONW"))
-    {
-      println("\n\n\nRETRY START CONFLUENT")
-      startConfluent = "sudo /opt/confluent-3.3.0/bin/confluent start schema-registry" !!
-    }
-
-    if(startConfluent.contains("DONW"))
-      fail("\n\n\nunable to start confluent")
-
-    "echo {\\\"key\\\": \\\"value\\\"} | sudo /opt/confluent-3.3.0/bin/kafka-avro-console-producer --broker-list localhost:9092 --topic test-topic --property value.schema='{\"type\":\"record\",\"name\":\"myrecord\",\"fields\":[{\"name\":\"key\",\"type\":\"string\"}]}'" !
+//
+//    println("\n\n\nSTART CONFLUENT")
+//
+//    var startConfluent = "sudo /opt/confluent-3.3.0/bin/confluent start schema-registry" !!
+//
+//    println("-----------")
+//    println(startConfluent)
+//    println("-----------")
+//
+//    if(startConfluent.contains("DONW"))
+//    {
+//      println("\n\n\nRETRY START CONFLUENT")
+//      startConfluent = "sudo /opt/confluent-3.3.0/bin/confluent start schema-registry" !!
+//    }
+//
+//    if(startConfluent.contains("DONW"))
+//      fail("\n\n\nunable to start confluent")
+//
+//    "echo {\\\"key\\\": \\\"value\\\"} | sudo /opt/confluent-3.3.0/bin/kafka-avro-console-producer --broker-list localhost:9092 --topic test-topic --property value.schema='{\"type\":\"record\",\"name\":\"myrecord\",\"fields\":[{\"name\":\"key\",\"type\":\"string\"}]}'" !
   }
 
   override def afterAll(): Unit = {
     super.afterAll()
 
-    if(ds != null)
-      ds.ssc.stop()
+//    if(ds != null)
+//      ds.ssc.stop()
   }
 
   "The Spark DirectStreamer" must
     "init spark conf and streming context" in {
 
-    ds = DirectStreamer()
-      .initStreaming("direct streaming test", "local", 10)
-
-    assert(ds.sc != null)
-    assert(ds.ssc != null)
+    pending
+//    ds = DirectStreamer()
+//      .initStreaming("direct streaming test", "local", 10)
+//
+//    assert(ds.sc != null)
+//    assert(ds.ssc != null)
   }
 
   it must "listen to a Kafka test-topic" in {
